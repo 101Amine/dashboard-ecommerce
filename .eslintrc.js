6738944c726269fea4521e3888a448e04a090cc2
101Amine <aminebecha8@gmail.com>
2023-01-07 18:04:46 +0100

    first commit

---
+++ .eslintrc.js
@@ -0,0 +1,23 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  extends: [
+    "eslint:recommended",
+    "plugin:react/recommended",
+    "plugin:prettier/recommended",
+  ],
+  overrides: [],
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+  },
+  plugins: ["react"],
+  rules: {
+    "react/react-in-jsx-scope": "off",
+    // Note: you must disable the base rule as it can report incorrect errors
+    "no-unused-vars": "off",
+  },
+};
